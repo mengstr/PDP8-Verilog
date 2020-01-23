2ff7a26697c2d4a21868d9ca6289cb6a8ed5179d
Mats Engstrom <mats.engstrom@gmail.com>
2020-01-23 13:47:28 +0100

    Cleanup of IRDECODER

--- IRDECODER.v
+++ IRDECODER.v
@@ -7,7 +7,7 @@ module IRDECODER  (
   output IND,
   output DIR,
   output MP,
-  output AAND ,
+  output AAND,
   output TAD,
   output ISZ,
   output DCA,
@@ -17,25 +17,22 @@ module IRDECODER  (
   output OPR
 );
 
-  wire s0,s1,s2;
-  wire or789;
-  assign or789=PCLATCHED[7] | PCLATCHED[8] | PCLATCHED[9];
-  assign OPR = (IR[10] & IR[11] & IR[9]);
-  assign s1 = ~ IR[11];
-  assign s0 = ~ IR[10];
-  assign s2 = ~ IR[9];
-  assign PPIND  = ((s0 & IR[3] & ~ IR[4] & ~ IR[5] & ~ IR[6] & IR[8] & ~ PCLATCHED[10] & ~ PCLATCHED[11] & ~ or789) | (s0 & IR[3] & ~ IR[4] & ~ IR[5] & ~ IR[6] & ~ IR[7] & IR[8]) | (s1 & IR[3] & ~ IR[4] & ~ IR[5] & ~ IR[6] & IR[8] & ~ PCLATCHED[10] & ~ PCLATCHED[11] & ~ or789) | (s1 & IR[3] & ~ IR[4] & ~ IR[5] & ~ IR[6] & ~ IR[7] & IR[8]));
-  assign IND = ((s0 & IR[7] & IR[8] & or789) | (s0 & IR[7] & IR[8] & PCLATCHED[10]) | (s0 & IR[7] & IR[8] & PCLATCHED[11]) | (s0 & ~ IR[3] & IR[8]) | (s0 & IR[4] & IR[8]) | (s0 & IR[5] & IR[8]) | (s0 & IR[6] & IR[8]) | (s1 & IR[7] & IR[8] & or789) | (s1 & IR[7] & IR[8] & PCLATCHED[10]) | (s1 & IR[7] & IR[8] & PCLATCHED[11]) | (s1 & ~ IR[3] & IR[8]) | (s1 & IR[4] & IR[8]) | (s1 & IR[5] & IR[8]) | (s1 & IR[6] & IR[8]));
-  assign DIR = ((s0 & ~ IR[8]) | (s1 & ~ IR[8]));
-  assign MP = ((s0 & IR[7]) | (s1 & IR[7]));
-  assign AAND  = (s0 & s1 & s2);
-  assign TAD = (s0 & s1 & IR[9]);
-  assign ISZ = (IR[10] & s1 & s2);
-  assign DCA = (IR[10] & s1 & IR[9]);
-  assign JMS = (s0 & IR[11] & s2);
-  assign JMP = (s0 & IR[11] & IR[9]);
-  assign IOT = (IR[10] & IR[11] & s2);
+  wire isPP1 = ~PCLATCHED[11] & ~PCLATCHED[10] & ~PCLATCHED[9] & ~PCLATCHED[8] & ~PCLATCHED[7];
+  wire isPP2 = ~IR[6] & ~IR[5] & ~IR[4] & IR[3];
+  wire isPP = (isPP1 | ~MP) & isPP2;
+
+  assign PPIND =  IR[8] &  isPP;
+  assign IND   =  IR[8] & ~isPP;
+  assign DIR   = ~IR[8];
+  assign MP    =  IR[7];
+
+  assign AAND = IR[11:9]==3'd0;
+  assign TAD  = IR[11:9]==3'd1;
+  assign ISZ  = IR[11:9]==3'd2;
+  assign DCA  = IR[11:9]==3'd3;
+  assign JMS  = IR[11:9]==3'd4;
+  assign JMP  = IR[11:9]==3'd5;
+  assign IOT  = IR[11:9]==3'd6;
+  assign OPR  = IR[11:9]==3'd7;
+
 endmodule
-
-
-
