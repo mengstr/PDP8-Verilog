b02d46224053e2578123ff4d51524320375662df
Mats Engstrom <mats.engstrom@gmail.com>
2020-02-10 23:13:13 +0100

    Fix hexing of wrong file

--- tools/makehexes.sh
+++ tools/makehexes.sh
@@ -10,7 +10,7 @@ T2H=tools/tape2hexram.sh
     echo Assembling $filename
     $PAL hex/src/$filename
     echo Convering $filename to hex
-    $T2H < hex/src/$filename > hex/src/$file.hex
+    $T2H < hex/src/$file.bin > hex/src/$file.hex
     mv -v -f hex/src/$file.hex hex/
     mv -v -f $(\ls -1 hex/src/$file.* | grep -v $filename) hex/tmp/
     echo ------------------------------------------------------------------------------
